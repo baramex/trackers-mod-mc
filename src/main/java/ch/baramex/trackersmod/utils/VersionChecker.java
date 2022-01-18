101f7c4c31d04c82d8412d8f6efbb06930f0d713
baramex <contact@baramex.me>
2022-01-18 22:19:27 +0100

    fixed version checker bugs

--- src/main/java/ch/baramex/trackersmod/utils/VersionChecker.java
+++ src/main/java/ch/baramex/trackersmod/utils/VersionChecker.java
@@ -1,14 +1,11 @@
 package ch.baramex.trackersmod.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -24,40 +21,37 @@ public class VersionChecker implements Runnable {
 	
 	@Override
 	public void run() {
-		final CloseableHttpClient httpClient = HttpClients.createDefault();
-		
-		HttpGet request = new HttpGet("https://api.github.com/repos/baramex/trackers-mod-mc/releases/latest");
-		
-		request.addHeader("Content-Type", "application/json");
-		
-		try (CloseableHttpResponse response = httpClient.execute(request)) {
-
-            HttpEntity entity = response.getEntity();
-            Header headers = entity.getContentType();
-
-            if (entity != null) {
-                String result = EntityUtils.toString(entity);
-                
-                JsonObject obj = new JsonParser().parse(result).getAsJsonObject();
-                
-                latestVersion = obj.get("tag_name").getAsString();
-                
-                if(obj.get("tag_name").getAsString().equals("v" + Reference.VERSION)) {
-                	isLatestVersion = true;
-                }
-                else {
-                	isLatestVersion = false;
-                }
-            }
-
-        } catch (Exception  e1) {
-			e1.printStackTrace();
-		}
-		
 		try {
-			httpClient.close();
-		} catch (IOException e1) {
-			e1.printStackTrace();
+			URL url = new URL("https://api.github.com/repos/baramex/trackers-mod-mc/releases/latest");
+	
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	
+			connection.setRequestProperty("accept", "application/json");
+			
+			if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+	
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+				
+				JsonObject obj = new JsonParser().parse(response.toString()).getAsJsonObject();
+				
+				latestVersion = obj.get("tag_name").getAsString();
+	            
+	            if(obj.get("tag_name").getAsString().equals("v" + Reference.VERSION)) {
+	            	isLatestVersion = true;
+	            }
+	            else {
+	            	isLatestVersion = false;
+	            }
+			}
+		}
+		 catch (IOException e) {
+				e.printStackTrace();
 		}
 	}
 	
